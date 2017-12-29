543896ab9973c35b380f69da8717578f12c8b1a0
leedongminAI <kid33629@gmail.com>
2017-12-30 02:25:18 +0900

    commit

--- RL_Lab_05_3.py
+++ RL_Lab_05_3.py
@@ -14,7 +14,7 @@
 # 하나의 예를 들자면, 한 명의 멘토가 있다고 합니다. 그 멘토의 말을 고집있게 들을 필요는 없습니다.
 # 왜냐하면 그 멘토는 자기가 와본 길(살았던 길)에 대해서만 설명해주기 때문입니다.
 # 우리는 때로는 우리의 고집대로 살아갈 필요가 있습니다. 우리의 인생은 우리가 선택하는 것이기 때문입니다.
-# 그러므로 멘토가 하는 말 10%듣고 내가 가고 싶은대로 90%를 하면됩니다.
+# 그러므로 멘토가 하는 말을 10%만 듣고 나머지는 내가 가고 싶은대로 방향으로 90%를 학습할 것입니다.
 # Listen to Q(s') (just a little bit)
 # Update Q(s) little bit (learning rate)
 
@@ -34,11 +34,32 @@ state_space_n = 16
 Q = np.zeros([state_space_n, action_space_n])
 
 # Set learning parameters
+# learning_rate = .95
+# learning_rate = .90
 learning_rate = .85
+# learning_rate = .80
+# learning_rate = .70
+# learning_rate = .65
+# learning_rate = .60
+# learning_rate = .50
+
 # 여기서는 빨리 학습을 시키고 싶어서 Q-learning에 85%를 주고, 내 고집을 15% 주겠습니다.
 # learning rate가 커지면 빨리 학습을 하고, 작아지면 천천히 학습을 합니다.
 
 # 왜 커지면 빨리 학습하고, 작아지면 천천히 학습하죠??
+# 먼저 밑에 있는 stochastic(non-deterministic) Q-learning Algorithm 공식을
+# Q[state, action] = (1 - learning_rate) * Q[state, action] \
+#    + learning_rate * (reward + dis * np.max(Q[new_state, :]))
+# 이 공식으로 보면 안되고 분배법칙을 적용한 공식으로 봐야합니다.
+
+# 즉 코드로 구현해보면,
+# Q[state, action] = Q[state, action] + learning_rate \
+#    * (reward + dis * np.max(Q[new_state, :]) - Q[state, action]) 으로 봐야합니다.
+
+# 여기서 learning rate 뒤에 있는 것을 TD error라고 합니다.(참고)
+
+# 따라서 밑에 분배법칙을 적용한 공식으로 보면 learning rate를 조절할 수 있습니다.
+# 하지만 너무 많이 올리면 overshooting이 될 수 있습니다. 따라서 조절하여 설정해야합니다.
 
 dis = .99
 num_episodes = 2000
@@ -62,8 +83,12 @@ for i in range(num_episodes):
 
         # Update Q-Table with new knowledge using learning rate
         # 이 부분이 바로 stochastic(non-deterministic) Q-learning Algorithm 공식을 적용한 코드입니다.
-        Q[state, action] = (1 - learning_rate) * Q[state, action] \
-            + learning_rate * (reward + dis * np.max(Q[new_state, :]))
+        # Q[state, action] = (1 - learning_rate) * Q[state, action] \
+            # + learning_rate * (reward + dis * np.max(Q[new_state, :]))
+
+        # 분배법칙을 적용한 공식을 코드로 표현하였습니다.
+        Q[state, action] = Q[state, action] + learning_rate \
+           * (reward + dis * np.max(Q[new_state, :]) - Q[state, action])
 
         rAll += reward
         state = new_state
